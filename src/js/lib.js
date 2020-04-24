40997d788318e7f8ffad7aa195af74f0c5592a1b
Osman <grupry@gmail.com>
2020-04-24 15:35:45 +0300

    implemented reviews block with reviews-slider

--- src/js/lib.js
+++ src/js/lib.js
@@ -10,9 +10,28 @@ $(document).ready(function() {
         smartSpeed: 1000,
         responsive: {
             0: {
-                items:1,
-                dots: true,
-                nav: false
+                items:1
+            },
+            480: {
+                items: 1
+            }
+        }
+    });
+})
+
+$(document).ready(function() {
+    $('.reviews-slider .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        dots: true,
+        navText: [],
+        autoplay: true,
+        autoplayHoverPause: true,
+        smartSpeed: 1000,
+        responsive: {
+            0: {
+                items:1
             },
             480: {
                 items: 1
